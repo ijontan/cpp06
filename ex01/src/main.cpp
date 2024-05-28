b52d197ded56fa47c304e2f115502c357c1dc8f0
ijontan <85871000+ijontan@users.noreply.github.com>
2024-05-28 15:14:17 +0800

    clean up

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -8,10 +8,10 @@ int main(void)
 	Data *data = new Data();
 	data->setvalue(2);
 
-	std::cout << "old data: " << data << std::endl;
+	std::cout << "old data: " << *data << ", pointer: " << data << std::endl;
 	uintptr_t raw = Serializer::serialize(data);
 	std::cout << "raw: " << raw << std::endl;
 	Data *ddata = Serializer::deserialize(raw);
-	std::cout << "new data: " << *ddata << std::endl;
+	std::cout << "new data: " << *ddata << ", pointer: " << ddata << std::endl;
 	delete data;
 }
